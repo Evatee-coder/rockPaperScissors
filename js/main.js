74199ca2272f9ecc3171154e03f18487b107e7e3
eva_tee15 <adetayo.eyelade2@gmail.com>
2022-07-26 14:43:32 -0400

    added playerScore and computerScore

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ const getComputerChoice = () => {
     const randNum = Math.floor(Math.random() * choiceArrays.length);
     return choiceArrays[randNum]; 
 }
-//console.log(getComputerChoice())
+
 const playRound = (playerSelection, computerSelection) => {
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    //console.log("3",playerSelection, "4", computerSelection)
+    
     if (playerSelection === "Rock" && computerSelection == "Scissors" || 
-    playerSelection === "Paper" && computerSelection == "Rock"   ||
-    playerSelection === "Scissors" && computerSelection == "Paper"){
+        playerSelection === "Paper" && computerSelection == "Rock"   ||
+        playerSelection === "Scissors" && computerSelection == "Paper"){
         playerScore++
         return `You won! ${playerSelection} beats ${computerSelection} `
         
@@ -33,17 +33,22 @@ const playRound = (playerSelection, computerSelection) => {
 //console.log(playRound(playerSelection, computerSelection))
 
 const game = () => {
-    for (let i = 0; i < 7; i++) {
-        console.log(i)
+    for (let i = 0; i < 5; i++) {
         const computerSelection = getComputerChoice();
         const playerSelection = prompt("Choose what to throw", "Rock, Paper, Scissors");
         console.log(playRound(playerSelection, computerSelection));
     }
     if (playerScore > computerScore) {
         return "You beat Computer mercilessly"
-    }else {
+        
+    }else if (playerScore < computerScore) {
         return "You got beaten by Computer Shame on you"
+        
+    }else {
+        return "You tied with computer, try more!"
     }
     
 }
 console.log(game())
+console.log(playerScore)
+console.log(computerScore)
